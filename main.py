3a88ccf54918b1d7d54820675eeeb8368996fea7
GabrielDaFonsecaNunes <fonseca.nunes1996@gmail.com>
2023-08-30 08:37:42 -0300

    Update Files

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from qdarktheme import load_stylesheet
 if __name__ == "__main__":
     app = QApplication([])
     # app.setStyleSheet(load_stylesheet("light"))
-    icon = QIcon("./resources/icon/icon.jpg")
-    app.setWindowIcon(icon)
+    # icon = QIcon("./resources/icon/icon.jpg")
+    # app.setWindowIcon(icon)
     window = MainWindow()
     window.show()
     app.exec()
